a7030e61dfd5ff57d317af1b29007d9c9f088400
DAKAEV Milana <120419173+dokibirdlover@users.noreply.github.com>
2024-12-06 09:28:35 +0100

    Add files via upload

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/chaima/projettrue/SFML_Project/Cell.cpp" "CMakeFiles/main.dir/Cell.cpp.o" "gcc" "CMakeFiles/main.dir/Cell.cpp.o.d"
+  "/home/chaima/projettrue/SFML_Project/Game.cpp" "CMakeFiles/main.dir/Game.cpp.o" "gcc" "CMakeFiles/main.dir/Game.cpp.o.d"
+  "/home/chaima/projettrue/SFML_Project/GameFile.cpp" "CMakeFiles/main.dir/GameFile.cpp.o" "gcc" "CMakeFiles/main.dir/GameFile.cpp.o.d"
+  "/home/chaima/projettrue/SFML_Project/Grid.cpp" "CMakeFiles/main.dir/Grid.cpp.o" "gcc" "CMakeFiles/main.dir/Grid.cpp.o.d"
+  "/home/chaima/projettrue/SFML_Project/Renderer.cpp" "CMakeFiles/main.dir/Renderer.cpp.o" "gcc" "CMakeFiles/main.dir/Renderer.cpp.o.d"
+  "/home/chaima/projettrue/SFML_Project/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
