57538fa02844ba73e8a58a45a24988893801a0cf
DAKAEV Milana <120419173+dokibirdlover@users.noreply.github.com>
2024-12-06 09:29:14 +0100

    Add files via upload

---
+++ CMakeFiles/main.dir/Cell.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/main.dir/Cell.cpp.o: \
+ /home/chaima/projettrue/SFML_Project/Cell.cpp /usr/include/stdc-predef.h \
+ /home/chaima/projettrue/SFML_Project/Cell.h
---
+++ CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/Cell.cpp.o"
+  "CMakeFiles/main.dir/Cell.cpp.o.d"
+  "CMakeFiles/main.dir/Game.cpp.o"
+  "CMakeFiles/main.dir/Game.cpp.o.d"
+  "CMakeFiles/main.dir/GameFile.cpp.o"
+  "CMakeFiles/main.dir/GameFile.cpp.o.d"
+  "CMakeFiles/main.dir/Grid.cpp.o"
+  "CMakeFiles/main.dir/Grid.cpp.o.d"
+  "CMakeFiles/main.dir/Renderer.cpp.o"
+  "CMakeFiles/main.dir/Renderer.cpp.o.d"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
