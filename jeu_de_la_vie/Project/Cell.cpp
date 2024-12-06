91ef0f2d8e2d4e7a6f2a92a717ba278b03406322
DAKAEV Milana <120419173+dokibirdlover@users.noreply.github.com>
2024-12-06 09:27:20 +0100

    Delete jeu_de_la_vie directory

--- jeu_de_la_vie/Project/Cell.cpp
+++
@@ -1,9 +0,0 @@
-#include "cell.h"
-
-Cell::Cell() : isAlive(false), isObstacle(false) {}
-
-void Cell::setState(bool state) { isAlive = state; }
-bool Cell::getState() const { return isAlive; }
-
-void Cell::setObstacle(bool obstacle) { isObstacle = obstacle; }
-bool Cell::getObstacle() const { return isObstacle; }
--- jeu_de_la_vie/Project/build/CMakeFiles/main.dir/Cell.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/main.dir/Cell.cpp.o: /mnt/d/projettruee/SFML_Project/Cell.cpp \
- /usr/include/stdc-predef.h /mnt/d/projettruee/SFML_Project/Cell.h
